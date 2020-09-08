e79cdfca0991c2b0043d6c377d8954c1eeaef2b6
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-09-08 10:04:31 +0200

    Fjern ubrukt fil

--- src/utils/urlUtils.ts
+++
@@ -1,7 +0,0 @@
-const url = window && window.location && window.location.href
-    ? window.location.href
-    : '';
-
-export const erProduksjon = () => {
-    return url.indexOf('www.nav.no') > -1;
-};
