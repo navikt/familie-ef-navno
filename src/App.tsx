d06c2a73c2104bd8b8cb4c3f2b6a88b93c108820
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-06-20 13:40:44 +0200

    Logg redirects
    Ser om de gamle sidene fortsatt treffes eller om det er trygt å ta ned
    appen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { logEvent } from "./utils/amplitude";
 
 function App() {
 
@@ -8,26 +9,32 @@ function App() {
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/overgangsstonad" component={() => { 
+              logEvent('besøk', {'redirect': 'overgangsstonad'})
               window.location.replace('https://www.nav.no/overgangsstonad-enslig'); 
               return null;
           }}/>
           <Route path="/barnetilsyn" component={() => { 
+              logEvent('besøk', {'redirect': 'barnetilsyn'})
               window.location.replace('https://www.nav.no/barnetilsyn-enslig'); 
               return null;
           }}/>
           <Route path="/skolepenger" component={() => { 
+              logEvent('besøk', {'redirect': 'skolepenger'})
               window.location.replace('https://www.nav.no/skolepenger-enslig'); 
               return null;
           }}/>
           <Route path="/tilleggsstonader" component={() => { 
+              logEvent('besøk', {'redirect': 'barnetilsyn'})
               window.location.replace('https://www.nav.no/tilleggsstonader-enslig'); 
               return null;
           }}/>
           <Route path="/hva-naa" component={() => { 
+              logEvent('besøk', {'redirect': 'hva-naa'})
               window.location.replace('https://www.nav.no/alene-med-barn'); 
               return null;
           }}/>
           <Route exact path="/" component={() => { 
+              logEvent('besøk', {'redirect': 'alene-med-barn'})
               window.location.replace('https://www.nav.no/alene-med-barn'); 
               return null;
           }}/>
