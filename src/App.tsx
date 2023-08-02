02641d563ec396031738bc10c61e19a88b94a691
Viktor <viktor.grondalen.solberg@nav.no>
2023-08-02 12:48:57 +0200

    slettet ubrukte assets, utils-filer og css/less-filer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { logEvent } from "./utils/amplitude";
+import { logEvent } from "./amplitude";
 
 function App() {
 
